419ad9be1c9b6c4af70e55b14a209c92fae48048
ALAEDDIN <94550523+yusefbahrami@users.noreply.github.com>
2022-11-20 21:42:28 +0330

    The sidebar functionality was customized

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const logoImage = document.querySelector(".logo-img");
 const profileImage = document.querySelector(".profile-image");
-window.onload = async () => {
+const sidebarHeader_uilBars = document.querySelector(
+  ".sidebar-header .uil-bars"
+);
+const sidebar = document.querySelector(".sidebar");
+
+const setImages = async () => {
   const logo = await fetch("./logo.svg");
   const profile = await fetch("./IMG_20221024_160602_501.jpg");
   logoImage.setAttribute("src", logo.url);
   profileImage.setAttribute("src", profile.url);
 };
+
+const steSidebarResponsive = () => {
+  sidebar.classList.toggle("minimal-sidebar");
+};
+sidebarHeader_uilBars.addEventListener("click", steSidebarResponsive);
+
+window.onload = () => {
+  setImages();
+  if (window.innerWidth >= 728) {
+    sidebar.classList.remove("minimal-sidebar");
+  }
+};
+window.addEventListener("resize", () => {
+  if (window.innerWidth <= 728) {
+    sidebar.classList.add("minimal-sidebar");
+  } else {
+    sidebar.classList.remove("minimal-sidebar");
+  }
+});
